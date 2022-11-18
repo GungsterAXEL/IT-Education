2a5d4bcb5b355126f8802c123bec2cbe86b9de9d
Gun`gster <gungster.axel.ru@mail.ru>
2022-11-18 21:25:11 +0300

    VerticalLine class added

--- GeekBrains/Digital Master 07.08.2022г/Отдельные курсы/Основы ООП/Snake/Program.cs
+++ GeekBrains/Digital Master 07.08.2022г/Отдельные курсы/Основы ООП/Snake/Program.cs
@@ -8,7 +8,10 @@ p1.Draw();
 Point p2 = new Point(4, 5, '#');
 p2.Draw();
 
-HorizontalLine line = new HorizontalLine(5,10,8,'+');
+HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
 line.Drow();
+
+VerticalLine line2 = new VerticalLine(8, 13, 10, '+');
+line2.Drow();
 
 Console.ReadLine();
