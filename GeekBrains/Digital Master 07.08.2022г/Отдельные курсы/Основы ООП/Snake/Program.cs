6c96918edf97a675e025da162a37a852f015db29
Gun`gster <gungster.axel.ru@mail.ru>
2022-11-18 21:16:03 +0300

    HorizontalLine constructor

--- GeekBrains/Digital Master 07.08.2022г/Отдельные курсы/Основы ООП/Snake/Program.cs
+++ GeekBrains/Digital Master 07.08.2022г/Отдельные курсы/Основы ООП/Snake/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Snake;
-using System.Drawing;
 
 Console.Clear();
 
@@ -9,29 +8,7 @@ p1.Draw();
 Point p2 = new Point(4, 5, '#');
 p2.Draw();
 
-List<int> numList = new List<int>();
-numList.Add(1);
-numList.Add(2);
-numList.Add(3);
-
-int x = numList[0],
-    y = numList[1],
-    z = numList[2];
-
-foreach (int i in numList)
-{
-    Console.WriteLine(i);
-}
-
-numList.RemoveAt(0);
-
-foreach (int i in numList)
-{
-    Console.WriteLine(i);
-}
-
-List<Point> plist = new List<Point>();
-plist.Add(p1);
-plist.Add(p2);
+HorizontalLine line = new HorizontalLine(5,10,8,'+');
+line.Drow();
 
 Console.ReadLine();
