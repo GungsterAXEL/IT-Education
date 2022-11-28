29a96b3c8eae25ff55e84aec460dfbd823bb00be
Gun`gster <gungster.axel.ru@mail.ru>
2022-11-28 22:16:48 +0300

    Python Webinar03 Examples added and solved

---
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_03/Example_01_Random_Number.py
@@ -0,0 +1,31 @@
+﻿'''
+1. Реализуйте алгоритм задания случайных чисел без использования встроенного
+   генератора псевдослучайных чисел.
+'''
+from time import time
+
+
+def random_number_generator():
+    random_number = time() * 1000 % 1000
+    return int(random_number)
+
+
+restart = ''
+while restart == '':
+    random_number = random_number_generator()
+    print(random_number)
+
+    restart = input('Рестарт?')
+
+'''
+Вариант из другого зала.
+
+from time import time
+
+def randfromtime(max):
+    time1 = time()
+    time1 = (time1 - int(time1))
+    return int(time1 * max)
+
+print(randfromtime(1000))
+'''
---
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_03/Example_02_Int_String_Finder.py
@@ -0,0 +1,36 @@
+﻿'''
+2. Задайте список. Напишите программу, которая определит, присутствует ли в 
+   заданном списке строк некое число.
+   ['22', '33', '123', 'fwefe', 'ytyy', '55']
+   
+   f(n)
+'''
+
+our_list = ['22', '33', '123', 'fwefe', 'ytyy', '55']
+u = 55
+
+print(our_list)
+
+# for i in range(len(our_list)):
+#    if our_list[i].isdigit() and int(our_list[i]) == u:
+#        print(f'В списке присутствует искомое значение.\nНа позиции {i}')
+
+for i in our_list:
+    if i.isdigit() and int(i) == u:
+        print(f'В списке присутствует искомое значение.\nНа позиции.')
+
+'''
+Решение Дениса.
+
+from random import randint
+import itertools
+
+n = 55
+    
+mylist = ['22', '33', '123', 'fwefe', 'ytyy', '55']
+
+def find_number(n, lst):
+    return str(n) in lst
+
+print(find_number(n, mylist))
+'''
