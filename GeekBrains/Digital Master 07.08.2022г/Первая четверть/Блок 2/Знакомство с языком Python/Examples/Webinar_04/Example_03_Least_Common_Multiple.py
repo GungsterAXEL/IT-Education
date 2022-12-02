e6ac85e0efa2c652a4f68f6e026f3f54c9ad623a
Gun`gster <gungster.axel.ru@mail.ru>
2022-12-03 02:38:10 +0300

    Python Webinar04 Examples solved

--- GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_04/Example_03_Least_Common_Multiple.py
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_04/Example_03_Least_Common_Multiple.py
@@ -1,60 +1,35 @@
 ﻿'''
-3. Задайте два числа. Напишите программу, которая найдёт НОК   (наименьшее общее кратное) этих двух чисел.
-
-   Алгоритм нахождения НОД делением
-   Большее число делим на меньшее.
-   Если делится без остатка, то меньшее число и есть НОД (следует выйти из цикла).
-   Если есть остаток, то большее число заменяем на остаток от деления.
-   Переходим к пункту 1.
+3. Задайте два числа. Напишите программу, которая найдёт НОК
+   (наименьшее общее кратное) этих двух чисел.
 '''
-a = 35
-b = 40
-bigger = b
-minimal = a
-n_o_d = 0
-count = 0
-if a > b:
-    bigger = a
-    minimal = b
+from random import randint
 
-if bigger % minimal == 0:
-    n_o_d = minimal
-    return
-else:
-    bigger = bigger % minimal
+restart = ''
+while restart == '':
+    a, b = randint(1, 50), randint(1, 50)
+    if a > b:
+        bigger = a
+        minimal = b
+    else:
+        bigger = b
+        minimal = a
 
+    i = 1
+    while i < b + 1:
+        print(i,':',i * a,':', i * b)
+        i += 1
 
-'''
-a, b = 3, 7
-maxx = max(a, b)
-i = maxx
-while True:
-    if i % a == 0 and i % b == 0:
-        print(i)
-        break    i += maxx
-'''
-'''
-dividing_list = []
-for i in input("Введите два числа: ").split():
-    dividing_list.append(int(i))
-
-product = dividing_list[0] * dividing_list[1]
-list1 = []
-list2 = []
-
-for i in range(dividing_list[0], product + 1, dividing_list[0]):
-    list1.append(i)
-
-for j in range(dividing_list[1], product + 1, dividing_list[1]):
-    list2.append(j)
-
-list3 = []
-
-for k in list1:
-    for l in list2:
-        if k == l:
-           list3.append(k)
-
-print(list1)
-print(list2)print(list3[0])
-'''
+    if bigger % minimal == 0:
+        print(f'Для чисел {a} и {b}.')
+        print('НОК = ', bigger)
+    elif bigger < 10 and minimal < 10:
+        print(f'Для чисел {a} и {b}.')
+        print('НОК = ', bigger * minimal)
+    else:
+        n_o_k = bigger
+        while (n_o_k % minimal) != 0:
+            n_o_k += bigger
+        print(f'Для чисел {a} и {b}.')
+        print('НОК = ', n_o_k)
+    
+    restart = input('Рестарт?')
