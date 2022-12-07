7f0701dd494b08f6a3e02efe70a398114e05b975
Gun`gster <gungster.axel.ru@mail.ru>
2022-12-08 00:40:48 +0300

    Python Webinar04 Homework solved

--- GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_04/Homework/Ex_05_Equation_Editor.py
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_04/Homework/Ex_05_Equation_Editor.py
@@ -4,104 +4,37 @@
     Коэффициенты могут быть как положительными, так и отрицательными.
     Степени многочленов могут отличаться.
 
-    6 5x^6 + 2x^4 + 66
+    5x^6 + 2x^4 + 66
     15x^6 + 12x^5 + 33x^2 - 66
 
-    2*x**3 + 5*x - 3
-
-    5*x**2 - 2*x + 4
-
-    2*x**3 + 5*x**2 + 3*x + 1
+    Уравнение 1: 2*x**3 + 5*x - 3
+    Уравнение 2: 5*x**2 - 2*x + 4
+    Результат:   2*x**3 + 5*x**2 + 3*x + 1
 '''
-eq1 = '50x**9 + 43x**7 + 3x**6 - 39x**4 - 63x**3 + 100x**2 + 82x + 101 = 0'
-eq2 = '43x**8 + 85x**7 + 17x**6 + 22x**4 + 57x**3 - 11x**2 - 6x - 89 = 0'
+from Hw_Methods import (file_reader as fr,
+                        polynomial_coefficient_extractor as pce,
+                        polynomial_list_converter as plc,
+                        dictionary_merger as dm,
+                        dictionary_equation_extractor as dee)
 
 
-#  Получение списка коэффициентов.
-eq1 = eq1.split('=')
-eq1 = eq1[0]
-eq1 = eq1.split()
-eqd = []
-for i in range(len(eq1)):
-    if eq1[i] == '-':
-        eqd.append(eq1[i] + eq1[i + 1])
-    elif eq1[i - 1] == '-':
-        pass
-    elif eq1[i] == '+':
-        pass
-    else:
-        eqd.append(eq1[i])
-print(eqd)
-#
-eq2 = eq2.split('=')
-eq2 = eq2[0]
-eq2 = eq2.split()
-eqf = []
-for i in range(len(eq2) - 1):
-    if eq2[i] == '-':
-        eqf.append(eq2[i] + eq2[i + 1])
-    elif eq2[i - 1] == '-':
-        pass
-    elif eq2[i] == '+':
-        pass
-    else:
-        eqf.append(eq2[i])
-print(eqf)
+# Чтение многочленов из файлов.
+equation = '.\GeekBrains\Digital Master 07.08.2022г\Первая четверть\Блок 2\Знакомство с языком Python\Examples\Webinar_04\Homework\Ex_04_Equation.txt'
+polynomial = '.\GeekBrains\Digital Master 07.08.2022г\Первая четверть\Блок 2\Знакомство с языком Python\Examples\Webinar_04\Homework\Ex_05_Polynomial.txt'
+edited_equation = '.\GeekBrains\Digital Master 07.08.2022г\Первая четверть\Блок 2\Знакомство с языком Python\Examples\Webinar_04\Homework\Ex_05_Result.txt'
 
 
-# Формирование словаря с иксами.
-eqrd = {}
-for index, value in enumerate(eqd):
-    if 'x**' in value:
-        s = value.split('x**')
-        s = f'x**{s[1]}'
-        eqrd[s] = int(value[:-len(s)])
-    elif 'x' in value:
-        s = 'x'
-        eqrd[s] = int(value[:-len(s)])
-    else:
-        eqrd['N'] = int(value)
-print('eqrd: ', eqrd)
-#
-eqrf = {}
-for index, value in enumerate(eqf):
-    if 'x**' in value:
-        s = value.split('x**')
-        s = f'x**{s[1]}'
-        eqrf[s] = int(value[:-len(s)])
-    elif 'x' in value:
-        s = 'x'
-        eqrf[s] = int(value[:-len(s)])
-    else:
-        eqrf['N'] = int(value)
-print('eqrf: ', eqrf)
+# Обработка многочленов, перевод в списки и конвертация в словари.
+equation = plc(pce(fr(equation)))
+polynomial = plc(pce(fr(polynomial)))
 
 
-# Объединение словарей.
-eqrs = eqd + eqf
-eqrr = {}
-for key, value in eqrd.items():
-    for jey, jalue in eqrf.items():
-        if key == jey:
-            eqrr[key] = value + jalue
-        elif key not in eqrf and jey not in eqrd:
-            eqrr[key] = value
-            eqrr[jey] = jalue
-print('eqrr: ', eqrr)
+# Объединение словарей и суммирование коэффициентов.
+polynomial_equation = dee(dm(equation, polynomial))
 
 
-# Результатирующая строка.
-result = ''
-for key, value in eqrr.items():
-    if key == 'N':
-        result += str(value) if value < 0 else '+' + str(value)
-    elif eqrr[key] > 0:
-        result += '+' + str(value) + str(key)
-    elif eqrr[key] < 0:
-        result += str(value) + str(key)
-if result[0] == '+':
-    result = result.replace('+', '', 1)
-result = result.replace('+', ' + ').replace('-', ' - ')
-print(eq1)
-print(eq2)
-print(result, '= 0')
+# Запись и чтение из файла.
+with open(edited_equation, 'w') as data:
+    data.write(polynomial_equation)
+with open(edited_equation, 'r') as data:
+    print(data.read())
--- GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_04/Homework/Hw_Methods.py
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_04/Homework/Hw_Methods.py
@@ -41,7 +41,8 @@ def equation_generator(degree_power):
                 equalation_list.append(
                     f'{random_number}x**{degree_power-i}' if random_number > 1 else f'x**{degree_power - i}')
             elif degree_power - i == 1:
-                equalation_list.append(f'{random_number}x' if random_number > 1 else f'x')
+                equalation_list.append(
+                    f'{random_number}x' if random_number > 1 else f'x')
             else:
                 if random_number != 0:
                     equalation_list.append(f'{random_number}')
@@ -63,9 +64,8 @@ def unique_value_finder(number_list):
         count = 0
     return unique_value_list
 
+
 # Вычисление π c заданной точностью.
-
-
 def pi_calculation(given_accuracy=0.00001):
     n = math.inf
     pi = 2 * (3**0.5)
@@ -112,3 +112,67 @@ def file_reader(file_name):
     with open(file_name, 'r') as file:
         content = file.read()
     return content
+
+
+# Работа с многочленами.
+#  Получение списка коэффициентов.
+def polynomial_coefficient_extractor(polynomial_string):
+    polynomial_string = polynomial_string.split('=')
+    polynomial_string = polynomial_string[0]
+    polynomial_string = polynomial_string.split()
+    polynomial_list = []
+    for i in range(len(polynomial_string)):
+        if polynomial_string[i] == '-':
+            polynomial_list.append(polynomial_string[i] + polynomial_string[i + 1])
+        elif polynomial_string[i - 1] == '-':
+            pass
+        elif polynomial_string[i] == '+':
+            pass
+        else:
+            polynomial_list.append(polynomial_string[i])
+    return polynomial_list
+
+# Формирование словаря с иксами.
+def polynomial_list_converter(polynomial_list):
+    polynomial_dictionary = {}
+    for index, value in enumerate(polynomial_list):
+        if 'x**' in value:
+            temp = value.split('x**')
+            temp = f'x**{temp[1]}'
+            polynomial_dictionary[temp] = int(value[:-len(temp)])
+        elif 'x' in value:
+            temp = 'x'
+            polynomial_dictionary[temp] = int(value[:-len(temp)])
+        else:
+            polynomial_dictionary['N'] = int(value)
+    return polynomial_dictionary
+
+
+# Результатирующая строка.
+def dictionary_equation_extractor(equation_dictionary):
+    equation_string = ''
+    for key, value in equation_dictionary.items():
+        if key == 'N':
+            equation_string += str(value) if value < 0 else '+' + str(value)
+        elif equation_dictionary[key] > 0:
+            equation_string += '+' + str(value) + str(key)
+        elif equation_dictionary[key] < 0:
+            equation_string += str(value) + str(key)
+    if equation_string[0] == '+':
+        equation_string = equation_string.replace('+', '', 1)
+    equation_string = equation_string.replace('+', ' + ').replace('-', ' - ')
+    return equation_string + ' = 0'
+
+
+# Объединение словарей.
+def dictionary_merger(dictionary_1, dictionary_2):
+    merged_dictionary = {}
+    for key, value in dictionary_1.items():
+        for jey, jalue in dictionary_2.items():
+            if key == jey:
+                merged_dictionary[key] = value + jalue
+            elif key not in dictionary_2 or jey not in dictionary_1:
+                merged_dictionary[key] = value
+                merged_dictionary[jey] = jalue
+    merged_dictionary = dict(sorted(merged_dictionary.items(), reverse= True))
+    return merged_dictionary
