7fd178b01476762545b093e123be9e2345aafdd7
Gun`gster <gungster.axel.ru@mail.ru>
2022-12-08 15:37:46 +0300

    Python Webinar05 homework update

---
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_05/Homework/Ex_04_WinRAR_Begining.py
@@ -0,0 +1,7 @@
+'''
+    4. ���������� RLE ��������: ���������� ������ ������ � �������������� ������.
+    ������� � �������� ������ �������� � ��������� ��������� ������.
+
+    aaaasssdddwwwwwwwwwwwweeeffffff -> 4a3s3d9w3w3e6f
+    4a3s3d9w3w3e6f-> aaaasssdddwwwwwwwwwwwweeeffffff
+'''
