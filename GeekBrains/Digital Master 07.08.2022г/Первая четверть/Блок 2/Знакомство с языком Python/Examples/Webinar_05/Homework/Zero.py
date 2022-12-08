8e7989efe34ca6d7abfd5e043c00da529eaf7e83
Gun`gster <gungster.axel.ru@mail.ru>
2022-12-08 20:16:49 +0300

    Python Webinar05 Hw update

---
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_05/Homework/Zero.py
@@ -0,0 +1,42 @@
+﻿from random import randint, choice
+
+
+def message_choice(message):
+    return choice(message)
+
+
+# Проверка выбора.
+def int_choice_range_checker(message, minimum_choice, maximum_choice):
+    out_message = choice(message)
+    user_choice = input(out_message)
+    try:
+        user_choice = int(user_choice)
+        if user_choice < minimum_choice or user_choice > maximum_choice:
+            print(f'Минимально возможный выбор: {minimum_choice}.\
+            \nМаксимальный: {maximum_choice}.')
+            user_choice = int_choice_range_checker(message, minimum_choice, maximum_choice)
+    except:
+        print('Ошибка ввода!')
+        user_choice = int_choice_range_checker(message, minimum_choice, maximum_choice)
+    finally:
+        return user_choice
+
+
+# "Проброс монетки".
+def coin_toss(message):
+    player_name = input(choice(message))
+    bot_name = ['(* ^ ω ^)', '(」°ロ°)」', '(‡▼益▼)', 'ʕ ᵔᴥᵔ ʔ']
+    if randint(0, 2) > 0:
+        player_1 = player_name
+        player_2 = choice(bot_name)
+    else:
+        player_1 = choice(bot_name)
+        player_2 = player_name
+    player_sequence = {player_1 : 0, player_2 : 0}
+    return player_sequence
+
+
+#
+#def game_process(coin_toss_result):
+#    restart = ''
+#    while restart == '':
