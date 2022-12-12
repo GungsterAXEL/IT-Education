e827916e028931d11292b6ba3bca2f17bee3ed85
Gun`gster <gungster.axel.ru@mail.ru>
2022-12-12 22:55:48 +0300

    Python Webinar07

---
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Знакомство с языком Python/Examples/Webinar_07/Calculator/User_Input.py
@@ -0,0 +1,5 @@
+﻿def user_input():
+    operation = input('Введите желаемый знак:')
+    n1 = complex(input("Введите первое число: "))
+    n2 = complex(input("Введите второе число: "))
+    return operation, n1, n2
