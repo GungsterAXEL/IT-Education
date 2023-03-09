b38752714a16155f319f9d4fb82b678c38df7a39
Gun`gster <gungster.axel.ru@mail.ru>
2023-03-10 02:51:30 +0300

    Java OOP Webinar_06 CreateDeleteResult modified

--- GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Объектно-ориентированное программирование/Examples/Webinar_02_03_04_05_06/Example_Zoo/terminal/CreateDeleteResult.java
+++ GeekBrains/Digital Master 07.08.2022г/Первая четверть/Блок 2/Объектно-ориентированное программирование/Examples/Webinar_02_03_04_05_06/Example_Zoo/terminal/CreateDeleteResult.java
@@ -2,12 +2,12 @@ package Webinar_02_03_04_05_06.Example_Zoo.terminal;
 
 import Webinar_02_03_04_05_06.Example_Zoo.animal.Animal;
 
-public class CreateDeleteResult {
-    public static <T extends Animal> void outcome(T animal) {
+public class CreateDeleteResult<T extends Animal> {
+    public static <T> void outcome(T animal) {
         System.out.println("Животное\n" + animal.toString() + "\nубрано из клетки.");
     }
 
-    public static <T extends Animal> void income(T animal) {
+    public static <T> void income(T animal) {
         System.out.println("Животное\n" + animal.toString() + "\nдобавлено в клетку.");
     }
 }
